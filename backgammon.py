989c4fbd2e70fcdb88a493dd94c83400ac0e6852
hakan <hakancaliskan95@gmail.com>
2019-07-11 11:07:08 +0300

    important doors
    important doors have defined

--- backgammon.py
+++ backgammon.py
@@ -2,5 +2,10 @@ def find_moves(checkers, dice1, dice2):
      #controling input
     if dice1<1 or dice1>6 or dice2<1 or dice2>6:
         return "Please try again"
+    
+    ##important doors have defined
+    important_doors = [5, 6, 7, 8, 17, 18, 19, 20]
+    filled = []
+    result = []
 
 print(find_moves(checkers,dice1,dice2))
