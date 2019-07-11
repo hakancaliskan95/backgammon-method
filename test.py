9f53227dd66834325ba7a2e85131b0e47913e43f
hakan <hakancaliskan95@gmail.com>
2019-07-11 14:47:33 +0300

    testing dices

--- test.py
+++ test.py
@@ -14,5 +14,15 @@ class test_moves(unittest.TestCase):
         result = backgammon.find_moves(self.checkers, self.dice1, self.dice2)
         print("First positions =>",self.checkers,"\nDices=>",self.dice1,self.dice2,"\nPossible Moves=>",result)
         
+    checkers = {1: 3, 6: 1, 10: 2, 12: 1, 13: 1}
+    dice1=7
+    dice2=2
+    positiion1=6
+    position2=10
+    
+    def runTest2(self):
+        result = backgammon.find_moves(self.checkers, self.dice1, self.dice2)
+        print("First positions =>",self.checkers,"\nDices=>",self.dice1,self.dice2,"\nPossible Moves=>",result)
+  
 if __name__ == "__main__":
     unittest.main()
