5b1d018e939f0c3285f43625ade3997044cf7a6a
hakan <hakancaliskan95@gmail.com>
2019-07-11 11:11:02 +0300

    possible moves
    finding possible moves

--- backgammon.py
+++ backgammon.py
@@ -22,5 +22,27 @@ def find_moves(checkers, dice1, dice2):
             filled.append(i)
             
             
+    #finding possible moves       
+    for key1 in checkers.keys():
+        position1 = key1+dice1
+        if position1>24:
+            break
+        for key2 in checkers.keys():
+            position2 = key2+dice2
+            if position2>24:
+                break
+            newCheckers = checkers.copy()
+            if position1 in newCheckers:
+                newCheckers[position1]+=1
+            else:
+                newCheckers[position1]=1
+            newCheckers[key1]-=1
+            if position2 in newCheckers:
+                newCheckers[position2]+=1
+            else:
+                newCheckers[position2]=1
+            newCheckers[key2]-=1
+            
+            
 checkers = {1: 3, 6: 1, 10: 2, 12: 1, 13: 1}   
 print(find_moves(checkers,dice1,dice2))
